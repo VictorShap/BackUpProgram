df6f77a0a19bdd041a47b29f1252c74a3be243e7
VictorShap <36379638+VictorShap@users.noreply.github.com>
2022-06-11 15:00:41 +0200

    dsdsa

--- WinFormsApp1/Methods.cs
+++ WinFormsApp1/Methods.cs
@@ -17,7 +17,7 @@ namespace WinFormsApp1
             {
                 path = folderBrowserDialog.SelectedPath;
             }
-            else path = "Не выбрано55555";
+            else path = "Не выбрано";
         }
         public static string EnoughSpaceOrThreeDots(string str, int max)
         {
