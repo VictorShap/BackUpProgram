c3ffe8f445aa86fee0e7113353828c88d024add0
Mr-Grenka <81305329+Mr-Grenka@users.noreply.github.com>
2022-06-11 17:26:18 +0300

    аааааааааааааа

--- WinFormsApp1/FormCopyingSettings.Designer.cs
+++ WinFormsApp1/FormCopyingSettings.Designer.cs
@@ -35,9 +35,7 @@ namespace WinFormsApp1
             this.TextBoxTypeExtension = new System.Windows.Forms.TextBox();
             this.LabelTypeExtension = new System.Windows.Forms.Label();
             this.CheckBoxCopyAllTheFiles = new System.Windows.Forms.CheckBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.LabelTips = new System.Windows.Forms.Label();
-            this.GroupGeneralSettings = new System.Windows.Forms.GroupBox();
             this.ListBoxDirectoriesResult = new System.Windows.Forms.ListBox();
             this.LabelTargetDirectory = new System.Windows.Forms.Label();
             this.LabelDirectoryToCopy = new System.Windows.Forms.Label();
@@ -50,38 +48,46 @@ namespace WinFormsApp1
             this.ButtonApply = new System.Windows.Forms.Button();
             this.ButtonCancel = new System.Windows.Forms.Button();
             this.ButtonOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.GroupAutoCopying.SuspendLayout();
-            this.GroupGeneralSettings.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.GroupAdditionalSettings.SuspendLayout();
             this.SuspendLayout();
             // 
             // GroupAutoCopying
             // 
+            this.GroupAutoCopying.Controls.Add(this.label2);
+            this.GroupAutoCopying.Controls.Add(this.label1);
+            this.GroupAutoCopying.Controls.Add(this.ListBoxDirectoriesResult);
             this.GroupAutoCopying.Controls.Add(this.TextBoxDaysToCopy);
+            this.GroupAutoCopying.Controls.Add(this.LabelTargetDirectory);
             this.GroupAutoCopying.Controls.Add(this.LabelDaysToCopy);
+            this.GroupAutoCopying.Controls.Add(this.LabelDirectoryToCopy);
+            this.GroupAutoCopying.Controls.Add(this.ButtonChooseTargetDirectory);
             this.GroupAutoCopying.Controls.Add(this.TextBoxTypeExtension);
+            this.GroupAutoCopying.Controls.Add(this.ButtonChooseDirectoryToCopy);
             this.GroupAutoCopying.Controls.Add(this.LabelTypeExtension);
             this.GroupAutoCopying.Controls.Add(this.CheckBoxCopyAllTheFiles);
-            this.GroupAutoCopying.Controls.Add(this.label6);
             this.GroupAutoCopying.Font = new System.Drawing.Font("Sitka Text", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.GroupAutoCopying.Location = new System.Drawing.Point(413, 27);
+            this.GroupAutoCopying.Location = new System.Drawing.Point(12, 27);
             this.GroupAutoCopying.Name = "GroupAutoCopying";
-            this.GroupAutoCopying.Size = new System.Drawing.Size(430, 153);
+            this.GroupAutoCopying.Size = new System.Drawing.Size(822, 127);
             this.GroupAutoCopying.TabIndex = 0;
             this.GroupAutoCopying.TabStop = false;
             this.GroupAutoCopying.Text = "Автоматическое копирование";
+            this.GroupAutoCopying.Enter += new System.EventHandler(this.GroupAutoCopying_Enter);
             // 
             // TextBoxDaysToCopy
             // 
-            this.TextBoxDaysToCopy.Location = new System.Drawing.Point(268, 83);
+            this.TextBoxDaysToCopy.Location = new System.Drawing.Point(651, 68);
             this.TextBoxDaysToCopy.Name = "TextBoxDaysToCopy";
             this.TextBoxDaysToCopy.Size = new System.Drawing.Size(100, 24);
             this.TextBoxDaysToCopy.TabIndex = 6;
             // 
             // LabelDaysToCopy
             // 
-            this.LabelDaysToCopy.Location = new System.Drawing.Point(6, 67);
+            this.LabelDaysToCopy.Location = new System.Drawing.Point(389, 52);
             this.LabelDaysToCopy.Name = "LabelDaysToCopy";
             this.LabelDaysToCopy.Size = new System.Drawing.Size(256, 40);
             this.LabelDaysToCopy.TabIndex = 1;
@@ -89,14 +95,14 @@ namespace WinFormsApp1
             // 
             // TextBoxTypeExtension
             // 
-            this.TextBoxTypeExtension.Location = new System.Drawing.Point(222, 35);
+            this.TextBoxTypeExtension.Location = new System.Drawing.Point(605, 20);
             this.TextBoxTypeExtension.Name = "TextBoxTypeExtension";
             this.TextBoxTypeExtension.Size = new System.Drawing.Size(146, 24);
             this.TextBoxTypeExtension.TabIndex = 5;
             // 
             // LabelTypeExtension
             // 
-            this.LabelTypeExtension.Location = new System.Drawing.Point(6, 35);
+            this.LabelTypeExtension.Location = new System.Drawing.Point(389, 20);
             this.LabelTypeExtension.Name = "LabelTypeExtension";
             this.LabelTypeExtension.Size = new System.Drawing.Size(211, 23);
             this.LabelTypeExtension.TabIndex = 0;
@@ -105,44 +111,21 @@ namespace WinFormsApp1
             // CheckBoxCopyAllTheFiles
             // 
             this.CheckBoxCopyAllTheFiles.AutoSize = true;
-            this.CheckBoxCopyAllTheFiles.Location = new System.Drawing.Point(6, 122);
+            this.CheckBoxCopyAllTheFiles.Location = new System.Drawing.Point(389, 98);
             this.CheckBoxCopyAllTheFiles.Name = "CheckBoxCopyAllTheFiles";
             this.CheckBoxCopyAllTheFiles.Size = new System.Drawing.Size(368, 23);
             this.CheckBoxCopyAllTheFiles.TabIndex = 7;
             this.CheckBoxCopyAllTheFiles.Text = "Копировать все файлы из выбранной директории";
             this.CheckBoxCopyAllTheFiles.UseVisualStyleBackColor = true;
             // 
-            // label6
-            // 
-            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label6.Location = new System.Drawing.Point(33, 117);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(300, 2);
-            this.label6.TabIndex = 3;
-            // 
             // LabelTips
             // 
             this.LabelTips.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.LabelTips.Location = new System.Drawing.Point(607, 186);
+            this.LabelTips.Location = new System.Drawing.Point(607, 160);
             this.LabelTips.Name = "LabelTips";
-            this.LabelTips.Size = new System.Drawing.Size(234, 167);
+            this.LabelTips.Size = new System.Drawing.Size(234, 151);
             this.LabelTips.TabIndex = 2;
             this.LabelTips.Text = "label1";
-            // 
-            // GroupGeneralSettings
-            // 
-            this.GroupGeneralSettings.Controls.Add(this.ListBoxDirectoriesResult);
-            this.GroupGeneralSettings.Controls.Add(this.LabelTargetDirectory);
-            this.GroupGeneralSettings.Controls.Add(this.LabelDirectoryToCopy);
-            this.GroupGeneralSettings.Controls.Add(this.ButtonChooseTargetDirectory);
-            this.GroupGeneralSettings.Controls.Add(this.ButtonChooseDirectoryToCopy);
-            this.GroupGeneralSettings.Font = new System.Drawing.Font("Sitka Text", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.GroupGeneralSettings.Location = new System.Drawing.Point(12, 27);
-            this.GroupGeneralSettings.Name = "GroupGeneralSettings";
-            this.GroupGeneralSettings.Size = new System.Drawing.Size(375, 153);
-            this.GroupGeneralSettings.TabIndex = 1;
-            this.GroupGeneralSettings.TabStop = false;
-            this.GroupGeneralSettings.Text = "Общие настройки";
             // 
             // ListBoxDirectoriesResult
             // 
@@ -150,7 +133,7 @@ namespace WinFormsApp1
             this.ListBoxDirectoriesResult.FormattingEnabled = true;
             this.ListBoxDirectoriesResult.HorizontalScrollbar = true;
             this.ListBoxDirectoriesResult.ItemHeight = 19;
-            this.ListBoxDirectoriesResult.Location = new System.Drawing.Point(8, 83);
+            this.ListBoxDirectoriesResult.Location = new System.Drawing.Point(6, 79);
             this.ListBoxDirectoriesResult.Name = "ListBoxDirectoriesResult";
             this.ListBoxDirectoriesResult.Size = new System.Drawing.Size(361, 42);
             this.ListBoxDirectoriesResult.TabIndex = 5;
@@ -158,7 +141,7 @@ namespace WinFormsApp1
             // 
             // LabelTargetDirectory
             // 
-            this.LabelTargetDirectory.Location = new System.Drawing.Point(6, 47);
+            this.LabelTargetDirectory.Location = new System.Drawing.Point(6, 50);
             this.LabelTargetDirectory.Name = "LabelTargetDirectory";
             this.LabelTargetDirectory.Size = new System.Drawing.Size(243, 23);
             this.LabelTargetDirectory.TabIndex = 3;
@@ -166,7 +149,7 @@ namespace WinFormsApp1
             // 
             // LabelDirectoryToCopy
             // 
-            this.LabelDirectoryToCopy.Location = new System.Drawing.Point(6, 20);
+            this.LabelDirectoryToCopy.Location = new System.Drawing.Point(6, 23);
             this.LabelDirectoryToCopy.Name = "LabelDirectoryToCopy";
             this.LabelDirectoryToCopy.Size = new System.Drawing.Size(233, 23);
             this.LabelDirectoryToCopy.TabIndex = 2;
@@ -175,7 +158,7 @@ namespace WinFormsApp1
             // ButtonChooseTargetDirectory
             // 
             this.ButtonChooseTargetDirectory.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ButtonChooseTargetDirectory.Location = new System.Drawing.Point(255, 46);
+            this.ButtonChooseTargetDirectory.Location = new System.Drawing.Point(255, 49);
             this.ButtonChooseTargetDirectory.Name = "ButtonChooseTargetDirectory";
             this.ButtonChooseTargetDirectory.Size = new System.Drawing.Size(72, 23);
             this.ButtonChooseTargetDirectory.TabIndex = 4;
@@ -185,7 +168,7 @@ namespace WinFormsApp1
             // ButtonChooseDirectoryToCopy
             // 
             this.ButtonChooseDirectoryToCopy.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ButtonChooseDirectoryToCopy.Location = new System.Drawing.Point(255, 20);
+            this.ButtonChooseDirectoryToCopy.Location = new System.Drawing.Point(255, 23);
             this.ButtonChooseDirectoryToCopy.Name = "ButtonChooseDirectoryToCopy";
             this.ButtonChooseDirectoryToCopy.Size = new System.Drawing.Size(72, 23);
             this.ButtonChooseDirectoryToCopy.TabIndex = 3;
@@ -199,7 +182,7 @@ namespace WinFormsApp1
             this.справкаToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(844, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(846, 24);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
@@ -214,9 +197,9 @@ namespace WinFormsApp1
             // 
             this.GroupAdditionalSettings.Controls.Add(this.CheckedListBoxAdditionalSettings);
             this.GroupAdditionalSettings.Font = new System.Drawing.Font("Sitka Text", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.GroupAdditionalSettings.Location = new System.Drawing.Point(12, 186);
+            this.GroupAdditionalSettings.Location = new System.Drawing.Point(12, 160);
             this.GroupAdditionalSettings.Name = "GroupAdditionalSettings";
-            this.GroupAdditionalSettings.Size = new System.Drawing.Size(589, 167);
+            this.GroupAdditionalSettings.Size = new System.Drawing.Size(589, 151);
             this.GroupAdditionalSettings.TabIndex = 1;
             this.GroupAdditionalSettings.TabStop = false;
             this.GroupAdditionalSettings.Text = "Дополнительные настройки";
@@ -239,7 +222,7 @@ namespace WinFormsApp1
             // ButtonApply
             // 
             this.ButtonApply.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ButtonApply.Location = new System.Drawing.Point(620, 356);
+            this.ButtonApply.Location = new System.Drawing.Point(628, 314);
             this.ButtonApply.Name = "ButtonApply";
             this.ButtonApply.Size = new System.Drawing.Size(67, 23);
             this.ButtonApply.TabIndex = 10;
@@ -249,7 +232,7 @@ namespace WinFormsApp1
             // ButtonCancel
             // 
             this.ButtonCancel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ButtonCancel.Location = new System.Drawing.Point(693, 356);
+            this.ButtonCancel.Location = new System.Drawing.Point(701, 314);
             this.ButtonCancel.Name = "ButtonCancel";
             this.ButtonCancel.Size = new System.Drawing.Size(67, 23);
             this.ButtonCancel.TabIndex = 10;
@@ -259,23 +242,40 @@ namespace WinFormsApp1
             // ButtonOk
             // 
             this.ButtonOk.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.ButtonOk.Location = new System.Drawing.Point(766, 356);
+            this.ButtonOk.Location = new System.Drawing.Point(774, 314);
             this.ButtonOk.Name = "ButtonOk";
             this.ButtonOk.Size = new System.Drawing.Size(67, 23);
             this.ButtonOk.TabIndex = 11;
             this.ButtonOk.Text = "Ок";
             this.ButtonOk.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(373, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 146);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(381, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2, 146);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
             // FormCopyingSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(844, 386);
+            this.ClientSize = new System.Drawing.Size(846, 339);
             this.Controls.Add(this.ButtonOk);
             this.Controls.Add(this.ButtonCancel);
             this.Controls.Add(this.ButtonApply);
             this.Controls.Add(this.GroupAdditionalSettings);
-            this.Controls.Add(this.GroupGeneralSettings);
             this.Controls.Add(this.LabelTips);
             this.Controls.Add(this.GroupAutoCopying);
             this.Controls.Add(this.menuStrip1);
@@ -286,7 +286,6 @@ namespace WinFormsApp1
             this.Text = "Настройки копирования";
             this.GroupAutoCopying.ResumeLayout(false);
             this.GroupAutoCopying.PerformLayout();
-            this.GroupGeneralSettings.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.GroupAdditionalSettings.ResumeLayout(false);
@@ -299,13 +298,11 @@ namespace WinFormsApp1
 
         private System.Windows.Forms.GroupBox GroupAutoCopying;
         private System.Windows.Forms.Label LabelTips;
-        private System.Windows.Forms.GroupBox GroupGeneralSettings;
         private System.Windows.Forms.Button ButtonChooseTargetDirectory;
         private System.Windows.Forms.Button ButtonChooseDirectoryToCopy;
         private System.Windows.Forms.Label LabelDirectoryToCopy;
         private System.Windows.Forms.Label LabelDaysToCopy;
         private System.Windows.Forms.Label LabelTypeExtension;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label LabelTargetDirectory;
         private System.Windows.Forms.CheckBox CheckBoxCopyAllTheFiles;
         private System.Windows.Forms.TextBox TextBoxDaysToCopy;
@@ -318,5 +315,7 @@ namespace WinFormsApp1
         private System.Windows.Forms.Button ButtonApply;
         private System.Windows.Forms.Button ButtonCancel;
         private System.Windows.Forms.Button ButtonOk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
